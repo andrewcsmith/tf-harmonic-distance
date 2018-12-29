6e558532d646500bfda4c8ddaa2c341b1b0a6565
Andrew C. Smith <andrewchristophersmith@gmail.com>
2018-12-29 15:08:39 -0800

    Add parabolic_loss_function

--- hd/optimize.py
+++ hd/optimize.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 
+from .utilities import reduce_parabola
+
 def stopping_op(loss, var_list, lr=1.0e-3, ct=1.0e-16):
     """
     Given a loss function and a variable list, gives a stopping condition Tensor that 
@@ -16,3 +18,12 @@ def stopping_op(loss, var_list, lr=1.0e-3, ct=1.0e-16):
         stopping_condition_op = tf.reduce_all(vals)
     
     return stopping_condition_op
+
+def parabolic_loss_function(pds, hds, log_pitches, a=1.0, b=1.0):
+    """
+    pds: Pitch distance coordinate values of each vector in the space
+    hds: Aggregate harmonic distance values of each vector in the space
+    log_pitches: The set of pitches to evaluate
+    """
+    distances = tf.map_fn(lambda x: reduce_parabola(pds - x, a=a, b=b), log_pitches)
+    return tf.reduce_min(distances * hds + hds, axis=1)

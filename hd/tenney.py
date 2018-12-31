1cec632027f3c0df560770f4ed11232913aa466b
Andrew C. Smith <andrewchristophersmith@gmail.com>
2018-12-30 21:29:50 -0800

    infer n_primes whenever possible

--- hd/tenney.py
+++ hd/tenney.py
@@ -8,19 +8,19 @@ from .vectors import *
 
 from tensorflow.python import debug as tf_debug
 
-def exploded_hd_graph(n_primes, vecs):
+def exploded_hd_graph(vecs):
+    n_primes = vecs.shape[-1]
     hd = tf.pow(tf.constant(PRIMES[:n_primes], dtype=tf.float64), vecs)
     return log2_graph(hd)
     
-def hd_graph(n_primes, vecs):
+def hd_graph(vecs):
     """
     Gets the harmonic distance of each row in a series of vecs
     """
-    hds = exploded_hd_graph(n_primes, vecs)
+    hds = exploded_hd_graph(vecs)
     return tf.reduce_sum(tf.abs(hds), axis=1)
 
 def hd_aggregate_graph(aggregates):
-    n_primes = aggregates.shape[-1]
     bases = get_bases(aggregates.shape[1] + 1)
     def get_hd_sum(hd):
         hd = tf.transpose(hd)
@@ -29,7 +29,7 @@ def hd_aggregate_graph(aggregates):
         hd = tf.reduce_sum(hd, axis=0)
         hd = tf.reduce_sum(hd, axis=0)
         return hd
-    hds = exploded_hd_graph(n_primes, aggregates)
+    hds = exploded_hd_graph(aggregates)
     return tf.map_fn(get_hd_sum, hds)
 
 def pd_aggregate_graph(aggregates):
@@ -40,8 +40,7 @@ def pd_aggregate_graph(aggregates):
 
 def scaled_hd_graph(log_pitches, vectors, c=0.05, coeff=E):
     scales = scales_graph(log_pitches, vectors, c=c, coeff=coeff) + 2.0e-32
-    n_primes = vectors.shape[-1]
-    hds = hd_graph(n_primes, vectors)
+    hds = hd_graph(vectors)
     hds = hds + 1.0
     hds = tf.tile(hds[:, None, None], [1, scales.shape[1], scales.shape[2]])
     hds = hds * tf.reciprocal(scales)

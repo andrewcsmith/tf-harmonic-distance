24d339da35f222f01d0d67d0736245b80d9f36e7
Andrew C. Smith <andrewchristophersmith@gmail.com>
2020-01-02 14:58:50 -0800

    test: Write tests for parabolic_loss_function

--- test_optimize.py
+++ test_optimize.py
@@ -18,3 +18,23 @@ def test_stopping_op():
         loss = hd.optimize.parabolic_loss_function(PDS, HDS, log_pitches, curves=(0.1, 0.1))
         return loss
     stopping_op = hd.optimize.stopping_op(loss, [log_pitches])
+
+def test_parabolic_loss_function_2d():
+    log_pitches = np.log2([[1.0, 1.0], [1.5, 1.0], [2.0, 1.0], [3.0, 1.0], [4.0, 1.0]])
+    vs = hd.vectors.VectorSpace(prime_limits=[3, 1], dimensions=2)
+    @tf.function
+    def loss(x, c=0.01):
+        return hd.optimize.parabolic_loss_function(vs.pds, vs.hds, x, curves=(0.01, 0.01))
+    exp = np.log2([1.0, 6, 2, 3, 4]) * 2
+    res = loss(log_pitches)
+    np.testing.assert_almost_equal(exp, res)
+
+def test_parabolic_loss_function_1d():
+    log_pitches = np.log2([1.0, 1.5, 2.0, 3.0, 4.0])
+    vs = hd.vectors.VectorSpace(prime_limits=[3, 1], dimensions=1)
+    @tf.function
+    def loss(x, c=0.01):
+        return hd.optimize.parabolic_loss_function(vs.pds, vs.hds, x, curves=(0.01))
+    exp = np.log2([1.0, 6, 2, 3, 4])
+    res = loss(log_pitches)
+    np.testing.assert_almost_equal(exp, res)

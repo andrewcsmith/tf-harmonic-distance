98e7b36a00135b62d60551f0a0035eef3558165f
Andrew C. Smith <andrewchristophersmith@gmail.com>
2020-01-03 16:52:02 -0800

    Add Minimizer class to optimize module

--- harmonic_distance/harmonic_distance/optimize.py
+++ harmonic_distance/harmonic_distance/optimize.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
+import datetime
 
 from .utilities import reduce_parabola
+from .vectors import VectorSpace
 
 def stopping_op(get_loss, var_list, lr=1.0e-3, ct=1.0e-16):
     """
@@ -27,3 +29,72 @@ def parabolic_loss_function(pds, hds, log_pitches, curves=None):
     distances = reduce_parabola(pds[:, None] - log_pitches, axis=len(log_pitches.shape), curves=curves)
     scaled = ((2.0 ** hds)[:, None] * distances) + hds[:, None]
     return tf.reduce_min(scaled, axis=0)
+
+class Minimizer(tf.Module):
+    def __init__(self, dimensions=1, learning_rate=1.0e-2, max_iters=1000, convergence_threshold=1.0e-5, c=0.01, **kwargs):
+        """
+        dimensions: Dimensionality of the space. This is only finding the
+        minimum of a single interval.
+        learning_rate: The learning rate of the optimization algorithm. A higher
+        value will converge more quickly, if possible, but might never converge.
+        max_iters: Maximum number of iterations before giving up on convergence.
+        convergence_threshold: The convergence threshold is the norm of the
+        gradients of the loss function. This is used to test whether a proper
+        "valley" has been found.
+        c: The "curve" of the parabola around each possible pitch. A higher
+        value will lead to fewer possible pitches.
+        """
+        self.dimensions = dimensions
+        self.learning_rate = learning_rate
+        self.max_iters = max_iters
+        self.convergence_threshold = convergence_threshold
+        self.curves = [c for _ in range(self.dimensions)]
+        self.vs = VectorSpace(**kwargs)
+        self.log_pitches = tf.Variable(tf.zeros((dimensions), dtype=tf.float64), dtype=tf.float64)
+        self.step = tf.Variable(0, dtype=tf.int64)
+    
+    def minimize(self):
+        self.step.assign(0)
+        self.writers = [self.timestamped_writer(var='/main')]
+        for idx in range(self.dimensions):
+            self.writers.append(self.timestamped_writer(var=("/pitch{}".format(idx+1))))
+        # Since Adagrad maintains state, we need to reset it at the start of each call to minimize()
+        self.opt = tf.optimizers.Adagrad(learning_rate=self.learning_rate)
+        self.write_values()
+        while self.stopping_op() and self.step < self.max_iters:
+            self.opt.minimize(lambda: self.loss(self.log_pitches), self.log_pitches)
+            self.step.assign_add(1)
+            self.write_values()
+        if self.stopping_op():
+            with self.writers[0].as_default():
+                tf.summary.text("convergence", "did not converge", step=self.step)
+        else:
+            with self.writers[0].as_default():
+                tf.summary.text("convergence", "converged", step=self.step)
+
+    def write_values(self):
+        current_loss = self.loss(self.log_pitches)
+        with self.writers[0].as_default():
+            tf.summary.scalar("loss", tf.reduce_mean(current_loss, axis=-1), step=self.step)
+            with tf.GradientTape() as g:
+                dz_dv = g.gradient(self.loss(self.log_pitches), self.log_pitches)
+            norms = tf.nn.l2_loss(dz_dv)
+            tf.summary.scalar("loss-norm", norms, step=self.step)
+        for idx, writer in enumerate(self.writers[1:]):
+            with writer.as_default():
+                tf.summary.scalar("loss", current_loss[idx], step=self.step)
+                tf.summary.scalar("pitch", self.log_pitches[idx], step=self.step)
+                
+    def timestamped_writer(self, var=''):
+        return tf.summary.create_file_writer('logs/fit/' + datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + var)
+
+    @tf.function
+    def loss(self, var_list):
+        return parabolic_loss_function(self.vs.pds, self.vs.hds, var_list, curves=self.curves)
+            
+    @tf.function
+    def stopping_op(self):
+        with tf.GradientTape() as g:
+            dz_dv = g.gradient(self.loss(self.log_pitches), self.log_pitches)
+        norms = tf.nn.l2_loss(dz_dv)
+        return norms >= self.convergence_threshold

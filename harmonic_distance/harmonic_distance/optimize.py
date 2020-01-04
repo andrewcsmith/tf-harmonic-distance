6cdc019b4683f822493201eeaaaf1de3ab507710
Andrew C. Smith <andrewchristophersmith@gmail.com>
2020-01-03 17:15:21 -0800

    Remove optimize.stopping_op in favor of Minimizer class

--- harmonic_distance/harmonic_distance/optimize.py
+++ harmonic_distance/harmonic_distance/optimize.py
@@ -4,22 +4,6 @@ import datetime
 from .utilities import reduce_parabola
 from .vectors import VectorSpace
 
-def stopping_op(get_loss, var_list, lr=1.0e-3, ct=1.0e-16):
-    """
-    Given a loss function and a variable list, gives a stopping condition Tensor that 
-    can be evaluated to see whether the variables have properly converged. 
-    """
-    opt = tf.optimizers.Adagrad(learning_rate=lr)
-
-    with tf.GradientTape() as g:
-        loss = get_loss()
-        dz_dv = g.gradient(loss, var_list)
-        # print("Gradients: ", dz_dv)
-    norms = tf.nn.l2_loss(dz_dv)
-    stop = norms < ct
-    opt.apply_gradients(zip(dz_dv, var_list))
-    return stop
-
 def parabolic_loss_function(pds, hds, log_pitches, curves=None):
     """
     pds: Pitch distance coordinate values of each vector in the space
@@ -94,6 +78,11 @@ class Minimizer(tf.Module):
             
     @tf.function
     def stopping_op(self):
+        """
+        Given a loss function and a variable list, gives a stopping condition
+        Tensor that can be evaluated to see whether the variables have properly
+        converged. 
+        """
         with tf.GradientTape() as g:
             dz_dv = g.gradient(self.loss(self.log_pitches), self.log_pitches)
         norms = tf.nn.l2_loss(dz_dv)
--- test_optimize.py
+++ test_optimize.py
@@ -8,17 +8,6 @@ if len(devices) > 0:
 import harmonic_distance as hd
 import numpy as np
 
-PDS = np.load("test_data/pds.npy")
-HDS = np.load("test_data/hds.npy")
-
-def test_stopping_op():
-    log_pitches = tf.Variable(tf.random.uniform([64, 2], 0.0, 1.0, dtype=tf.float64))
-    @tf.function
-    def loss():
-        loss = hd.optimize.parabolic_loss_function(PDS, HDS, log_pitches, curves=(0.1, 0.1))
-        return loss
-    stopping_op = hd.optimize.stopping_op(loss, [log_pitches])
-
 def test_parabolic_loss_function_2d():
     log_pitches = np.log2([[1.0, 1.0], [1.5, 1.0], [2.0, 1.0], [3.0, 1.0], [4.0, 1.0]])
     vs = hd.vectors.VectorSpace(prime_limits=[3, 1], dimensions=2)

c51acf141437924b5492cbd0f18ad5e146421692
Andrew C. Smith <andrewchristophersmith@gmail.com>
2020-01-03 18:40:46 -0800

    Reorganize for conda-build and conda-install

--- harmonic_distance/setup.py
+++ harmonic_distance/setup.py
@@ -1,10 +1,14 @@
 import setuptools
+import os
 
-with open("README.md", "r") as fh:
+dirname = os.path.dirname(__file__)
+readme = os.path.join(dirname, 'README.md')
+
+with open(readme, "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="harmonic_distnace",
+    name="harmonic_distance",
     version="0.0.1",
     author="Andrew C. Smith",
     author_email="andrewchristophersmith@gmail.com",
